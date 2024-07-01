9a325bad096afcaa87fdd13c0c18a74ec76f33bd
EliasMez <elias.mezine@laposte.net>
2024-07-01 15:01:57 +0200

    xpath and items added

--- project_scrapy_js/items.py
+++ project_scrapy_js/items.py
@@ -8,3 +8,15 @@ import scrapy
 
 class FilmItem(scrapy.Item):
     title = scrapy.Field()
+    titre_original = scrapy.Field()
+    score = scrapy.Field()
+    genre = scrapy.Field()
+    date = scrapy.Field()
+    duree = scrapy.Field()
+    descriptions = scrapy.Field()
+    acteurs = scrapy.Field()
+    realisateur = scrapy.Field()
+    public = scrapy.Field()
+    pays = scrapy.Field()
+    url_image = scrapy.Field()
+    langue = scrapy.Field()
--- project_scrapy_js/spiders/allocinespider.py
+++ project_scrapy_js/spiders/allocinespider.py
@@ -9,7 +9,7 @@ class AllocinespiderSpider(CrawlSpider):
     start_urls = ["https://www.allocine.fr/film/meilleurs"]
 
     rules = (
-        Rule(LinkExtractor(restrict_xpaths="//h2/a"), callback='parse_item', follow=False),
+        Rule(LinkExtractor(restrict_xpaths="//h2/a"), callback='parse_item', follow=False), process_request='use_playwright',
         Rule(LinkExtractor(restrict_xpaths="//span[@class='txt' and text()='Suivante']/.."), follow=True, process_request='use_playwright'),
     )
 
@@ -24,6 +24,21 @@ class AllocinespiderSpider(CrawlSpider):
     def parse_item(self, response):
         item = FilmItem()
         item['title'] = response.xpath("//div[@class='titlebar-title titlebar-title-xl']/text()").get()
+        item["titre_original"] = ''.join(response.xpath("//div[@class='meta-body-item']/span[contains(text(), 'Titre original')]/following-sibling::span[1]/text()").extract())
+        item["score"] = ''.join(response.xpath("//div[@class='rating-item']/div/a[contains(text(), ' Spectateurs ')]/../div/span[@class='stareval-note']/text()").extract())
+        item["genre"] = ', '.join(response.xpath("//span[@class='spacer'][2]/following-sibling::a/text()").extract())
+        item["date"] = ''.join(response.xpath("//a[@class='xXx date blue-link']/text()").extract())
+        if item["date"] == "":
+            item["date"] = ''.join(response.xpath("//span[@class='date']/text()").extract())
+        item["duree"] = ''.join(response.xpath("//span[@class='spacer'][1]/following-sibling::text()[1]").extract())
+        item["descriptions"] = ''.join(response.xpath("//p[@class='bo-p']/text()").extract())
+        item["acteurs"] = ', '.join(response.xpath("//div/span[contains(text(), 'Avec')]/../a/text()").extract())
+        item["realisateur"] = ''.join(response.xpath("//div/span[contains(text(), 'De')]/../a/text()").extract())
+        item["public"] = ''.join(response.xpath("//div[@class='certificate']/span[@class='certificate-text']/text()").extract())
+        item["pays"] = ''.join(response.xpath("//a[@class='xXx nationality']/text()").extract())
+        item["url_image"] = ''.join(response.xpath("//div[@class='card entity-card entity-card-list cf entity-card-player-ovw']/figure/a/img/@src").extract())
+        item["langue"] = ', '.join(response.xpath("//div[@class='item']/span[contains(text(), 'Langues')]/following-sibling::span[1]/text()").extract())
+        
         yield item
 
 
